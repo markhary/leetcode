6292c5d09cec646ef84cbf328538fd461fe879b1
Mark Hary <44713017+markhary@users.noreply.github.com>
2020-12-25 16:58:45 -0800

    Use array instead of vector, improved efficiency

--- src/sudokuSolver.cpp
+++ src/sudokuSolver.cpp
@@ -7,6 +7,7 @@
 //
 
 #include <iostream>
+#include <array>
 #include <vector>
 
 #include <gtest/gtest.h>
@@ -20,10 +21,12 @@ using namespace std;
 struct Cell
 {
     // Also keyed for N=9 and assuming numeric Sudoku
-    vector<char> candidates{'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
-    int row;
-    int column;
+    int n;
+    int value{UNSOLVED};
 };
+
+typedef vector<vector<char>> Chars;
+typedef array<array<Cell, 9>, 9> Cells;
 
 class Sudoku
 {
@@ -33,24 +36,33 @@ public:
     {
         _M = _R * _C;
         _N = _M * _M;
-        _cells = vector<vector<Cell>>(_M, vector<Cell>(_M));
     }
 
-    void display(vector<vector<char>> board)
+    void display(Cells &board, bool code)
     {
+        cout << (code ? "{" : "");
         for (int n = 0; n < _N; n++)
         {
             int r = n2r(n);
             int c = n2c(n);
-            cout << board[r][c] << " ";
+            if (c == 0)
+            {
+                cout << (code ? "{" : "");
+            }
+            cout << (code ? "'" : "")
+                 << board[r][c].value
+                 << (code ? "'," : "")
+                 << " ";
             if (c == (_M - 1))
             {
+                cout << (code ? "}," : "");
                 cout << endl;
             }
         }
+        cout << (code ? "}" : "") << endl;
     }
 
-    bool isValid(vector<vector<char>> &board, int r, int c)
+    bool isValid(Cells &board, int r, int c)
     {
         try
         {
@@ -73,33 +85,34 @@ public:
         return true;
     }
 
-    void from(vector<vector<char>> &board)
+    void from(Chars &board)
     {
-        _board = board;
         for (int n = 0; n < _N; n++)
         {
             int r = n2r(n);
             int c = n2c(n);
 
             char value = board[r][c];
-            _cells[r][c].row = r;
-            _cells[r][c].column = c;
-            if (value != UNSOLVED)
-            {
-                _cells[r][c].candidates.clear();
-            }
+            _cells[r][c].n = n;
+            _cells[r][c].value = value;
         }
     }
 
-    void to(vector<vector<char>> &board)
+    void to(Chars &board)
     {
-        board = _board;
+        for (int n = 0; n < _N; n++)
+        {
+            int r = n2r(n);
+            int c = n2c(n);
+
+            board[r][c] = _cells[r][c].value;
+        }
     }
 
     void solve()
     {
-        int count = countUnsolved(_board);
-        _board = solve(_board, count);
+        int count = countUnsolved(_cells);
+        _cells = solve(_cells, count);
         _solved = true;
     }
 
@@ -110,7 +123,7 @@ public:
 
     bool check()
     {
-        return check(_board);
+        return (!countUnsolved(_cells));
     }
 
 private:
@@ -129,34 +142,34 @@ private:
         return (r / _R) * _R + (c / _C);
     }
 
-    vector<vector<char>> solve(vector<vector<char>> board, int unsolved)
+    Cells solve(Cells board, int unsolved, int n_0 = 0)
     {
-        vector<vector<char>> b = board;
+        Cells b = board;
 
-        for (int n = 0; n < _N; n++)
+        for (int n = n_0; n < _N; n++)
         {
             int r = n2r(n);
             int c = n2c(n);
 
-            if (b[r][c] != UNSOLVED)
+            if (b[r][c].value != UNSOLVED)
             {
                 continue;
             }
 
             for (int i = 0; i < _M; i++)
             {
-                b[r][c] = 0x31 + i;
+                b[r][c].value = 0x31 + i;
 
                 if (isValid(b, r, c))
                 {
-                    int n = unsolved - 1;
-                    if (n == 0)
+                    int u = unsolved - 1;
+                    if (u == 0)
                     {
                         _solved = true;
                     }
                     else
                     {
-                        b = solve(b, n);
+                        b = solve(b, u, n);
                     }
                     if (_solved)
                     {
@@ -170,29 +183,14 @@ private:
         return board;
     }
 
-    bool check(vector<vector<char>> &board)
-    {
-        for (int n = 0; n < _N; n++)
-        {
-            int r = n2r(n);
-            int c = n2c(n);
-
-            if (board[r][c] == UNSOLVED)
-            {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    int countUnsolved(vector<vector<char>> &board)
+    int countUnsolved(Cells &board)
     {
         int count = 0;
         for (int n = 0; n < _N; n++)
         {
             int r = n2r(n);
             int c = n2c(n);
-            if (board[r][c] == UNSOLVED)
+            if (board[r][c].value == UNSOLVED)
             {
                 count++;
             }
@@ -223,19 +221,19 @@ private:
         values |= (1 << v);
     }
 
-    bool checkRow(int row, int column, vector<vector<char>> &board, int &values)
+    bool checkRow(int row, int column, Cells &board, int &values)
     {
-        checkValue(board[row][column], values);
+        checkValue(board[row][column].value, values);
         return true;
     }
 
-    bool checkColumn(int row, int column, vector<vector<char>> &board, int &values)
+    bool checkColumn(int row, int column, Cells &board, int &values)
     {
-        checkValue(board[row][column], values);
+        checkValue(board[row][column].value, values);
         return true;
     }
 
-    char getCell(int box, vector<vector<char>> &board, int i)
+    char getCell(int box, Cells &board, int i)
     {
         int firstRow = (box / _R) * _R;
         int firstCol = (box % _C) * _C;
@@ -243,10 +241,10 @@ private:
         int row = firstRow + i / _R;
         int col = firstCol + (i % _C);
 
-        return board[row][col];
+        return board[row][col].value;
     }
 
-    bool checkBox(int box, int cell, vector<vector<char>> &board, int &values)
+    bool checkBox(int box, int cell, Cells &board, int &values)
     {
         char c = getCell(box, board, cell);
         checkValue(c, values);
@@ -254,8 +252,7 @@ private:
     }
 
 private:
-    vector<vector<char>> _board;
-    vector<vector<Cell>> _cells;
+    Cells _cells;
     int _R;
     int _C;
     int _M;
@@ -267,16 +264,16 @@ private:
 class Solution
 {
 public:
-    void solveSudoku(vector<vector<char>> &board)
+    void solveSudoku(Chars &board)
     {
         _sudoku.from(board);
         _sudoku.solve();
         _sudoku.to(board);
     }
 
-    void display(vector<vector<char>> &board)
+    void display(Cells &board, bool code = false)
     {
-        _sudoku.display(board);
+        _sudoku.display(board, code);
     }
 
 private:
@@ -286,7 +283,7 @@ private:
 TEST(SudokuSolver, Solved)
 {
     Sudoku sudoku;
-    vector<vector<char>> board =
+    Chars board =
         {{'5', '3', '4', '6', '7', '8', '9', '1', '2'},
          {'6', '7', '2', '1', '9', '5', '3', '4', '8'},
          {'1', '9', '8', '3', '4', '2', '5', '6', '7'},
@@ -304,7 +301,7 @@ TEST(SudokuSolver, Solved)
 TEST(SudokuSolver, NotSolved)
 {
     Sudoku sudoku;
-    vector<vector<char>> board =
+    Chars board =
         {{'.', '3', '4', '6', '7', '8', '9', '1', '2'},
          {'6', '7', '2', '1', '9', '5', '3', '4', '8'},
          {'1', '9', '8', '3', '4', '2', '5', '6', '7'},
@@ -322,7 +319,7 @@ TEST(SudokuSolver, NotSolved)
 TEST(SudokuSolver, Simple)
 {
     Solution solution;
-    vector<vector<char>> board =
+    Chars board =
         {{'.', '3', '4', '6', '7', '8', '9', '1', '2'},
          {'6', '7', '2', '1', '9', '5', '3', '4', '8'},
          {'1', '9', '8', '3', '4', '2', '5', '6', '7'},
@@ -333,7 +330,7 @@ TEST(SudokuSolver, Simple)
          {'2', '8', '7', '4', '1', '9', '6', '3', '5'},
          {'3', '4', '5', '2', '8', '6', '1', '7', '9'}};
 
-    vector<vector<char>> solved =
+    Chars solved =
         {{'5', '3', '4', '6', '7', '8', '9', '1', '2'},
          {'6', '7', '2', '1', '9', '5', '3', '4', '8'},
          {'1', '9', '8', '3', '4', '2', '5', '6', '7'},
@@ -351,7 +348,7 @@ TEST(SudokuSolver, Simple)
 TEST(SudokuSolver, TooSimple)
 {
     Solution solution;
-    vector<vector<char>> board =
+    Chars board =
         {{'.', '.', '4', '6', '7', '8', '9', '1', '2'},
          {'6', '7', '2', '1', '9', '5', '3', '4', '8'},
          {'1', '9', '8', '3', '4', '2', '5', '6', '7'},
@@ -362,7 +359,7 @@ TEST(SudokuSolver, TooSimple)
          {'2', '8', '7', '4', '1', '9', '6', '3', '5'},
          {'3', '4', '5', '2', '8', '6', '1', '7', '9'}};
 
-    vector<vector<char>> solved =
+    Chars solved =
         {{'5', '3', '4', '6', '7', '8', '9', '1', '2'},
          {'6', '7', '2', '1', '9', '5', '3', '4', '8'},
          {'1', '9', '8', '3', '4', '2', '5', '6', '7'},
@@ -380,7 +377,7 @@ TEST(SudokuSolver, TooSimple)
 TEST(SudokuSolver, Example)
 {
     Solution solution;
-    vector<vector<char>> board =
+    Chars board =
         {{'5', '3', '.', '.', '7', '.', '.', '.', '.'},
          {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
          {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
@@ -391,7 +388,7 @@ TEST(SudokuSolver, Example)
          {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
          {'.', '.', '.', '.', '8', '.', '.', '7', '9'}};
 
-    vector<vector<char>> solved =
+    Chars solved =
         {{'5', '3', '4', '6', '7', '8', '9', '1', '2'},
          {'6', '7', '2', '1', '9', '5', '3', '4', '8'},
          {'1', '9', '8', '3', '4', '2', '5', '6', '7'},
@@ -401,6 +398,35 @@ TEST(SudokuSolver, Example)
          {'9', '6', '1', '5', '3', '7', '2', '8', '4'},
          {'2', '8', '7', '4', '1', '9', '6', '3', '5'},
          {'3', '4', '5', '2', '8', '6', '1', '7', '9'}};
+
+    solution.solveSudoku(board);
+    ASSERT_TRUE(board == solved);
+}
+
+TEST(SudokuSolver, Test1)
+{
+    Solution solution;
+    Chars board =
+        {{'.', '.', '.', '.', '.', '7', '.', '.', '9'},
+         {'.', '4', '.', '.', '8', '1', '2', '.', '.'},
+         {'.', '.', '.', '9', '.', '.', '.', '1', '.'},
+         {'.', '.', '5', '3', '.', '.', '.', '7', '2'},
+         {'2', '9', '3', '.', '.', '.', '.', '5', '.'},
+         {'.', '.', '.', '.', '.', '5', '3', '.', '.'},
+         {'8', '.', '.', '.', '2', '3', '.', '.', '.'},
+         {'7', '.', '.', '.', '5', '.', '.', '4', '.'},
+         {'5', '3', '1', '.', '7', '.', '.', '.', '.'}};
+
+    Chars solved =
+        {{'3', '1', '2', '5', '4', '7', '8', '6', '9'},
+         {'9', '4', '7', '6', '8', '1', '2', '3', '5'},
+         {'6', '5', '8', '9', '3', '2', '7', '1', '4'},
+         {'1', '8', '5', '3', '6', '4', '9', '7', '2'},
+         {'2', '9', '3', '7', '1', '8', '4', '5', '6'},
+         {'4', '7', '6', '2', '9', '5', '3', '8', '1'},
+         {'8', '6', '4', '1', '2', '3', '5', '9', '7'},
+         {'7', '2', '9', '8', '5', '6', '1', '4', '3'},
+         {'5', '3', '1', '4', '7', '9', '6', '2', '8'}};
 
     solution.solveSudoku(board);
     ASSERT_TRUE(board == solved);
